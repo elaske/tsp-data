0867111630ba8e7de89e5d20b25b35cbbf3263d0
Evan Laske <evan.laske@gmail.com>
2014-07-26 20:29:48 -0400

    Adding PDFQuery as a submodule for use in developing a TSP statement parser.

---
+++ pdfquery_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Evan Laske
+# @Date:   2014-07-26 20:22:51
+# @Last Modified by:   Evan Laske
+# @Last Modified time: 2014-07-26 20:29:28
+
+import StringIO
+import sys
+import pdfquery
+
