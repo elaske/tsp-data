3c25644d5643f8b05ec4d463ad4dfad85cee41e1
Evan Laske <evan.laske@gmail.com>
2014-12-25 02:23:47 -0500

    "Functionized" the common methods to the algorithm; cleaned up some print statements and unnecessary functions.

--- pdfquery_test.py
+++ pdfquery_test.py
@@ -16,29 +16,10 @@ from pdftables.boxes import Box, Rectangle
 
 # from pdfquery.cache import FileCache, DummyCache
 
-def groupElements(elements, kind):
-    """
-    Group a list of elements in a table area by the kind (row / col).
-    """
-    # Group all of the elements into columns
-    # Start with an empty list of columns
-    groups = []
-    for e in elements:
-        # Grab an element, check to see if it overlaps any X in the list. 
-        overlaps = [group for group in groups if group.centerOverlaps(e)]
-        # If it doesn't match any of them / empty list of groups
-        if len(overlaps) == 0:
-            # Create a new one for us to use
-            groups.append( LineGrouping(kind, e) )
-            print e.layout
-        else:
-            # if overlaps[0].allEncapsulateElement(e):
-            # default to the first one even if there are more than one.
-            overlaps[0].checkAndAdd(e)
-    return groups
-
 def groupElementsByEdges(kind, elements, edges):
-
+    """
+    Groups the given elements between the edges and direction given.
+    """
     # Create the line groupings to start throwing things together into
     groups = [LineGrouping(kind, a, b) for a,b in zip(edges, edges[1:])]
 
@@ -52,6 +33,11 @@ def groupElementsByEdges(kind, elements, edges):
     return groups
 
 def zipGroupsIntoTable(rows, columns):
+    """
+    Takes two LineGroupings (one for the rows and one for the columns) and puts
+    them together into a table. Both LineGroupings must include all elements of
+    the table.
+    """
     # Sort the rows and columns; columns increase left to right, rows increase bottom to top
     rows = sorted(rows, key=lambda r: r.minExtreme, reverse=True)
     columns = sorted(columns, key=lambda c: c.minExtreme)
@@ -79,6 +65,13 @@ def zipGroupsIntoTable(rows, columns):
     return table
 
 def condenseElements(table):
+    """
+    Condenses the given table's list of elements in each cell together.
+
+    If there are multiple elements in a "cell", this attempts to order them how
+    they were in the source document, then combining them into a single string.
+    It will also remove all unnecessary whitespace from the strings.
+    """
     newTable = []
     for r in table:
         l = []
@@ -96,64 +89,83 @@ def condenseElements(table):
     return newTable
 
 def printTable(table):
+    """
+    Prints a given table (list of rows) on multiple lines, pseudo-table-like
+    """
     for r in table:
         print r
 
-def findAccountSummary(pdf):
+def findTableByName(pdf, name, marker):
     """
-    Finds the account summary within the document. Must provide a loaded PDF document.
+    Find a table by the given name (title) and end marker within the given 
+    loaded pdf.
+
+    Returns a list of found tables in the form of:
+        [(page : bbox), ...]
     """
-    # The title box will have define the top of the table.
-    table_title = pdf.pq('LTTextLineHorizontal:contains("YOUR QUARTERLY ACCOUNT SUMMARY")')
+    # Find all of the table names on all the pages
+    # TODO: find a way to do this without using the "hidden" _pages attribute.
+    table_titles = [pdf.pq('LTPage[page_index="{}"] LTTextLineHorizontal:contains("{}")'.format(i, name)) for i in range(len(pdf._pages))]
+    # print table_titles
 
-    print type(table_title)
+    # If there weren't any found, we can short-circuit the rest.
+    if not any(table_titles):
+        return None
 
-    print table_title.attr('x0'), table_title.attr('y0'), table_title.attr('x1'), table_title.attr('y1')
+    # Find all the marker locations within the document by page
+    # TODO: Change this to only have to look at the pages we find titles on?
+    markers = [pdf.pq('LTPage[page_index="{}"] LTTextLineHorizontal:contains("{}")'.format(i, marker)) for i in range(len(pdf._pages))]
+    # Match up the marker locations and the table title locations
+    pairs = [(x,y) for x,y in zip(table_titles,markers) if x and y]
+    # If none were found, exit gracefully
+    if not pairs:
+        return None
 
-    # The table's top left is at x0, y0 of this label
-    top = float(table_title.attr('y0'))
-    left = float(table_title.attr('x0'))
+    return_list = []
+    # Otherwise, I guess we'll have to do it for all of them...
+    for i, pair in enumerate(zip(table_titles,markers)):
+        titles, markers = pair
+        # Handle each title individually
+        for title in titles:
+            # The table's top left is at x0, y0 of the title label (not including the title itself)
+            top = title.layout.y0
+            left = title.layout.x0
+            # If there is more than one ending marker in the list 
+            if len(markers) > 1:
+                bottom = 0
+                # Compare all the markers and find the closest one.
+                for e in markers:
+                    # do the actual updating of the bottom of the bbox
+                    if abs(top - float(e.layout.y0)) < abs(top - bottom):
+                        bottom = float(e.layout.y0)
+            else:
+                bottom = markers[0].layout.y0
+            # Make the right the same margin as the left (11" paper, 72PPI)
+            # TODO: get this information from the page through a query.
+            right = 11 * 72 - left
+            return_list.append( (i, [left, bottom, right, top]) )
 
-    total_lines = pdf.pq('LTPage[page_index="1"] LTTextLineHorizontal:contains("Total")')
+    return return_list
 
-    print total_lines
+def getTableElements(pdf, table):
+    """
+    Returns the elements from the table given by (page : bbox).
+    """
+    page, bbox = table
+    return pdf.pq(
+        'LTPage[page_index="{}"] LTTextLineHorizontal:in_bbox("{}")'.format(
+            page, ','.join([str(x) for x in bbox])
+            )
+        )
 
-    bottom = 0
-
-    print len(total_lines)
-
-    # Find the total line that is closest to the table heading. This only works for the account summary.
-    if len(total_lines) > 1:
-        for e in total_lines:
-            print type(e.layout)
-            print e.layout.__dict__
-            # The layout attribute of the element gets the LayoutElement from the etree.
-            print e.layout
-            if abs(top - float(e.layout.y0)) < abs(top - bottom):
-                bottom = float(e.layout.y0)
-            print e.layout.y0
-    else:
-        bottom = total_lines[0].layout.y0
-
-    print bottom
-
-    # Make the right the same margin as the left (11" paper, 72PPI)
-    right = 11 * 72 - left
-
-    print [left, bottom, right, top]
-
-    elements = pdf.pq('LTPage[page_index="1"] LTTextLineHorizontal:in_bbox("%s, %s, %s, %s")' % (left, bottom, right, top))
-
-    print elements
-    print len(elements)
-    print dir(elements[0].layout)
-    print elements[0].text
-
-    # groups = groupElements(elements, 'col')
-
-    # #print groups
-    # for g in groups:
-    #     print [str(e.layout).split("'")[1] for e in g.sortedElements()]
+def binTableElements(table, elements, row_threshold=1, col_threshold=3):
+    """
+    Takes in elements from the given table and returns them grouped into rows
+    and columns. These can be zipped together into a real table.
+    """
+    # Unpack bbox
+    page, bbox = table 
+    bb = Box(Rectangle(*bbox))
 
     x_segments = [LineSegment(e.layout.x0, e.layout.x1, e) for e in elements]
     y_segments = [LineSegment(e.layout.y0, e.layout.y1, e) for e in elements]
@@ -161,19 +173,17 @@ def findAccountSummary(pdf):
     xh = x_boundaries, x_item_counts = segment_histogram(x_segments)
     yh = y_boundaries, y_item_counts = segment_histogram(y_segments)
 
-    print x_boundaries
-    print len([x for x in x_item_counts if x > 2])
+    # print x_boundaries
+    # print len([x for x in x_item_counts if x > 2])
 
-    print x_item_counts.count(0)
+    # print x_item_counts.count(0)
 
-    bb = Box(Rectangle(left, bottom, right, top))
-
-    xh = above_threshold(xh, 3)
-    yh = above_threshold(yh, 1)
+    xh = above_threshold(xh, col_threshold)
+    yh = above_threshold(yh, row_threshold)
 
     col_edges, row_edges = compute_cell_edges(bb, xh, yh, None)
-    print len(col_edges), col_edges
-    print len(row_edges), row_edges
+    # print len(col_edges), col_edges
+    # print len(row_edges), row_edges
 
     # for a,b in zip(col_edges, col_edges[1:]):
     #     print a, b, b-a
@@ -184,17 +194,7 @@ def findAccountSummary(pdf):
     columns = groupElementsByEdges('col',elements,col_edges)
     rows = groupElementsByEdges('row',elements,row_edges)
 
-    table = zipGroupsIntoTable(rows, columns)
-
-    table = condenseElements(table)
-
-    printTable(table)
-
-    # Debug:
-    # for s in [str([e.layout for e in group.sortedElements()]) for group in columns]:
-    #     print s
-    # for s in [str([e.layout for e in group.sortedElements()]) for group in rows]:
-    #     print s
+    return rows, columns
 
 def main():
     """
@@ -205,9 +205,15 @@ def main():
                                  )
     pdf.load()
 
-    findAccountSummary(pdf)
-
-    # pdf.tree.write("test2.xml", pretty_print=True, encoding="utf-8")
+    tables = findTableByName(pdf, 'YOUR QUARTERLY ACCOUNT SUMMARY', 'Total')
+    # print tables
+    for t in tables:
+        elements = getTableElements(pdf, t)
+        # print len(elements)
+        rows, columns = binTableElements(t, elements)
+        table = zipGroupsIntoTable(rows, columns)
+        table = condenseElements(table)
+        printTable(table)
 
 if __name__ == '__main__':
     main()
